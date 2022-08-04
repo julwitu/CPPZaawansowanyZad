1fa4e65413ea1e34d562a6a7627bcce30de1d0f2
Julia Witucka <jwitucka@gmail.com>
2022-08-04 16:34:38 +0200

    Zad9

--- Zad9/Zad9.cpp
+++ Zad9/Zad9.cpp
@@ -11,10 +11,26 @@ std::vector<int> createNumbers(int to) {
    // std::for_each(numbers.begin(), numbers.end(), [](int i) {std::cout << i << " "; });
     std::vector<int>multiple2;
     std::vector<int>multiple3;
-    std::copy_if(numbers.begin(), numbers.end(), std::back_inserter(multiple2), [](int i) {});
-    
-    std::for_each(multiple2.begin(), multiple2.end(), [](int i) {std::cout << i << " "; });
-    return numbers;
+   std::for_each(numbers.begin(), numbers.end(), [&multiple2](int i) {
+        if (i % 2 == 0) {
+            multiple2.push_back(i);
+        }
+        });
+    std::for_each(numbers.begin(), numbers.end(), [&multiple3](int i) {
+        if (i % 3 == 0) {
+            multiple3.push_back(i);
+        }
+        });
+    //TODO remove_copy_if
+
+    std::vector<int>multiple2and3;
+    // znajduje elementy powtarzajace sie w obu wektorach i wypycha je do nowego wektora
+    std::set_intersection(multiple2.begin(), multiple2.end(), 
+        multiple3.begin(), multiple3.end(),
+        std::back_inserter(multiple2and3));
+
+    std::for_each(multiple2and3.begin(), multiple2and3.end(), [](int i) {std::cout << i << " "; });
+    return multiple2and3;
 }
 
 int main()
